08b125c6ec87b82a92cdbf5a41d02db1ee044466
Azara-Nya <77995029+Azara-Nya@users.noreply.github.com>
2022-07-24 12:45:07 +0100

    Health Script

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,21 +7,48 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     public bool TimerUp;
+    public int health=3;
     [SerializeField] private float MaxTime;
     [SerializeField] private TextMeshProUGUI ScoreText;
+    [SerializeField] private GameObject[] hearts;
     private float CurrentTime;
 
     void Update()
     {
         if(CurrentTime >= MaxTime)
         {
+            if(health!=0 && TimerUp==false)
+            {
+                health -=1;
+            }
             TimerUp=true;
-            Debug.Log("Minus 1 Heart");
         }
         else
         {
             CurrentTime += Time.fixedDeltaTime/7;
         }
         ScoreText.text = $"{Math.Round(CurrentTime)}";
+
+        if(health==2)
+        {
+            hearts[2].SetActive(false);
+        }
+        if(health ==1)
+        {
+            hearts[1].SetActive(false);
+        }
+        if(health<=0)
+        {
+            hearts[0].SetActive(false);
+            Debug.Log("GameOver");
+            Debug.Log("Display Score yada yada");
+        }
+
+
+    if(TimerUp && health !=0)
+    {
+        //Load Next Scene;
     }
+    }
+
 }
